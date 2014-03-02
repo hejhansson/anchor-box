4fde94b49337c922ba1039f70f1f28430694fab8
hejhansson <hejhansson@me.com>
2014-03-02 16:04:31 +0100

    404 page

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php theme_include('header'); ?>
 
-	<section class="content wrap">
-		<h1>Page not found</h1>
+	<div class="container">
+		<h1>Sidan kunde inte hittas</h1>
 
-		<p>Unfortunately, the page <code>/<?php echo current_url(); ?></code> could not be found. Your best bet is either to try the <a href="<?php echo base_url(); ?>">homepage</a>, try <a href="#search">searching</a>, or go and cry in a corner (although I don’t recommend the latter).</p>
-	</section>
+		<p><code>/<?php echo current_url(); ?></code> finns inte. Bättre lycka nästa gång. </p>
+	</div>
 
 <?php theme_include('footer'); ?>
